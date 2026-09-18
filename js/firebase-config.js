9379bc6038b5f613003d4b4bf40a0f6f52cef0e8
k100ed10-alt <k100ed10@gmail.com>
2026-09-18 08:51:28 +0400

    Point login codes to Firebase project grade12platform

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+/* مشروع Firebase: grade12platform
+   انسخ القيم من Firebase Console ← Project settings ← Your apps ← Web */
+window.FIREBASE_CONFIG = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "grade12platform.firebaseapp.com",
+  projectId: "grade12platform",
+  storageBucket: "grade12platform.firebasestorage.app",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
